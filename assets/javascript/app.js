7944e55a0b2f3974ed120218c476f648b0c8e0d0
Rick <rick.carnes@gmail.com>
2019-06-19 11:19:20 -0400

    layout game screen complete-needs js game function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var questions = {
         q1: {
-            question: 'What is the name of Jon`s direwolf?',
+            question: 'What is the name of Jon\'s direwolf?',
             choices: {
                 c1: "Grey Wind",
                 c2: "Summer",
@@ -9,7 +9,7 @@ $(document).ready(function() {
                 c4: "Ghost"
             },
             correctAnswer: "Ghost",
-            answerDef: 'Ghost, unlike the other direwolves found by the Starks, is an albino described by Theon Greyjoy as "the runt of the litter". Nymeria was Arya`s direwolf whom she freed in the Riverlands. Otherwise, Nymeria would be killed for attacking Joffrey. Grey Wind and Summer, both deceased, were Robb and Bran`s direwolves respectively.'
+            answerDef: 'Ghost, unlike the other direwolves found by the Starks, is an albino described by Theon Greyjoy as "the runt of the litter". Nymeria was Arya\'s direwolf whom she freed in the Riverlands. Otherwise, Nymeria would be killed for attacking Joffrey. Grey Wind and Summer, both deceased, were Robb and Bran\'s direwolves respectively.'
         },
 
         q2: {
@@ -21,11 +21,11 @@ $(document).ready(function() {
                 c4: "Waters",
             },
             correctAnswer: "Flowers",
-            answerDef: 'The bastards born in noble families can be identified by their different surnames, according to the region where they were born. In The Reach they receive the name of Flowers, probably a reference to the characteristics of the region, since it`s a fertile land.'
+            answerDef: 'The bastards born in noble families can be identified by their different surnames, according to the region where they were born. In The Reach they receive the name of Flowers, probably a reference to the characteristics of the region, since it\'s a fertile land.'
         },
 
         q3: {
-            question: 'Which House is a direct vassal of House Baratheon of King`s Landing?',
+            question: 'Which House is a direct vassal of House Baratheon of King\'s Landing?',
             choices: {
                 c1: "Stokeworth",
                 c2: "Upcliff",
@@ -33,7 +33,7 @@ $(document).ready(function() {
                 c4: "Karstark",
             },
             correctAnswer: "Stokeworth",
-            answerDef: 'Stokeworth is a vassal House to the House Baratheon of King`s Landing, and since its castle is located in the Crownlands, they are directly loyal to the monarch. We can see, for instance, Bronn walking with Lollys Stokeworth moments before Jaime Lannister hires him in order to rescue Myrcella from the Martells in Dorne.'
+            answerDef: 'Stokeworth is a vassal House to the House Baratheon of King\'s Landing, and since its castle is located in the Crownlands, they are directly loyal to the monarch. We can see, for instance, Bronn walking with Lollys Stokeworth moments before Jaime Lannister hires him in order to rescue Myrcella from the Martells in Dorne.'
         },
 
         q4: {
@@ -45,11 +45,11 @@ $(document).ready(function() {
                 c4: "Aerys Targaryen because he killed Robert's brother",
             },
             correctAnswer: "Rhaegar Targaryen because he kidnapped Lyanna Stark",
-            answerDef: 'While Robert is visiting Winterfell, he visits the memorial of Lyanna Stark located down in the crypts. Lyanna, Ned Stark`s sister, was promised to Robert before she was kidnapped by Rhaegar Targaryen. Robert started the rebellion because she was taken from him. Fans of the show will notice hints that Lyanna was not actually kidnapped, but she ran away to be with Rhaegar.',
+            answerDef: 'While Robert is visiting Winterfell, he visits the memorial of Lyanna Stark located down in the crypts. Lyanna, Ned Stark\'s sister, was promised to Robert before she was kidnapped by Rhaegar Targaryen. Robert started the rebellion because she was taken from him. Fans of the show will notice hints that Lyanna was not actually kidnapped, but she ran away to be with Rhaegar.',
         },
 
         q5: {
-            question: 'At Hoster Tully`s funeral, who shot the burning arrow that hit its mark?',
+            question: 'At Hoster Tully\'s funeral, who shot the burning arrow that hit its mark?',
             choices: {
                 c1: "Brynden Tully",
                 c2: "Robb Stark",
@@ -57,7 +57,7 @@ $(document).ready(function() {
                 c4: "Lord Karstark",
             },
             correctAnswer: "Brynden Tully",
-            answerDef: 'Edmure Tully had the right to shoot the arrow. However, after many unsuccessful attempts Brynden Tully took the bow for his brother`s last rite, setting the floating man afire.',
+            answerDef: 'Edmure Tully had the right to shoot the arrow. However, after many unsuccessful attempts Brynden Tully took the bow for his brother\'s last rite, setting the floating man afire.',
         },
 
         q6: {
@@ -69,7 +69,7 @@ $(document).ready(function() {
                 c4: "Yoren",
             },
             correctAnswer: "Yoren",
-            answerDef: 'In season 2, Yoren was killed by Ser Amory Lorch after Yoren refused to give Ser Amory the location of Gendry. Gendry was one of King Robert`s bastards, but Yoren defended him because he was going to be a brother of the Night`s Watch.',
+            answerDef: 'In season 2, Yoren was killed by Ser Amory Lorch after Yoren refused to give Ser Amory the location of Gendry. Gendry was one of King Robert\'s bastards, but Yoren defended him because he was going to be a brother of the Night\'s Watch.',
         },
 
         q7: {
@@ -81,11 +81,11 @@ $(document).ready(function() {
                 c4: "Bran Stark",
             },
             correctAnswer: "Theon Greyjoy",
-            answerDef: 'In season 2, Tyrion tells Grand Maester Pycelle, Varys, and Petyr Baelish that he is planning on arranging the marriage of Princess Myrcella. Tyrion`s goal was to figure out if any of the fellow Small Council members are spying for Cersei. Tyrion tells Grand Maester Pyrcelle (who turns out to be the spy) that he is planning to marry Princess Myrcella to the Martells of Dorne, he tells Varys he is planning on marrying Myrcella to Theon Greyjoy, and he tells Littlefinger that he is planning to marry Myrcella to Robin Arryn of the Vale. When Tyrion is called into Cersei`s chambers later that night, Cersei accuses him of plotting to send away her only daughter to Dorne. At that moment, Tyrion knows that Pycelle is the spy, and after he leaves Cersei`s chambers he finds Bronn and they pay Pycelle a very special visit.',
+            answerDef: 'In season 2, Tyrion tells Grand Maester Pycelle, Varys, and Petyr Baelish that he is planning on arranging the marriage of Princess Myrcella. Tyrion\'s goal was to figure out if any of the fellow Small Council members are spying for Cersei. Tyrion tells Grand Maester Pyrcelle (who turns out to be the spy) that he is planning to marry Princess Myrcella to the Martells of Dorne, he tells Varys he is planning on marrying Myrcella to Theon Greyjoy, and he tells Littlefinger that he is planning to marry Myrcella to Robin Arryn of the Vale. When Tyrion is called into Cersei\'s chambers later that night, Cersei accuses him of plotting to send away her only daughter to Dorne. At that moment, Tyrion knows that Pycelle is the spy, and after he leaves Cersei\'s chambers he finds Bronn and they pay Pycelle a very special visit.',
         },
 
         q8: {
-            question: 'How many fingertips did Stannis Baratheon chop off of Davos` hand(s)?',
+            question: 'How many fingertips did Stannis Baratheon chop off of Davos\' hand(s)?',
             choices: {
                 c1: "Three",
                 c2: "Six",
@@ -93,7 +93,7 @@ $(document).ready(function() {
                 c4: "Four",
             },
             correctAnswer: "Four",
-            answerDef: 'Davos was a smuggler for the bigger part of his life. He was well known for being able to slip past sentries. One day the castle Storm`s End was under siege and Stannis Baratheon, who was defending the castle, ran out of his food supplies. That was when Davos slipped past the enemy`s lines and got into Storm`s End with lots of food, mainly onions. For this, Stannis Baratheon rewarded Davos with lands and knighthood. Davos choose `Seaworth` for his house`s name. Unfortunately, Stannis also chopped off four of Davos` fingertips for his smuggling days.',
+            answerDef: 'Davos was a smuggler for the bigger part of his life. He was well known for being able to slip past sentries. One day the castle Storm\'s End was under siege and Stannis Baratheon, who was defending the castle, ran out of his food supplies. That was when Davos slipped past the enemy\'s lines and got into Storm\'s End with lots of food, mainly onions. For this, Stannis Baratheon rewarded Davos with lands and knighthood. Davos choose "Seaworth" for his house\'s name. Unfortunately, Stannis also chopped off four of Davos\' fingertips for his smuggling days.',
         },
 
         q9: {
@@ -120,13 +120,27 @@ $(document).ready(function() {
             answerDef: 'Almost all of the continent, with the exception of the Wall and lands beyond, is under the rule of the Seven Kingdoms. Westeros contains a variety of terrain including forests, mountains and deserts.',
         },
     }
+    $('#question').hide();
+    $('#c1').hide();
+    $('#c2').hide();
+    $('#c3').hide();
+    $('#c4').hide();
     $('#startBtn').on("click", function() {
-        $('body').css("background", "url('../images/nightKing.jpg')");
+        $('body').css("background", "url('assets/images/nightKing.jpg') no-repeat center center fixed");
+        $('body').css("-webkit-background-size", "cover");
+        $('body').css("-moz-background-size", "cover");
+        $('body').css("-o-background-size", "cover");
+        $('body').css("background-size", "cover");
         $('#banner').hide();
         $('#bannerText').hide();
         $('#bannerH1').hide();
         $('#bannerH3').hide();
         $('#startBtn').hide();
-        $('#audio').append('<source src="assets/audio/themeSong.mp3"></source>')
+        $('body').append('<audio id="audio" autoplay loop><source src="assets/audio/themeSong.mp3"></source></audio>');
+        $('#question').show();
+        $('#c1').show();
+        $('#c2').show();
+        $('#c3').show();
+        $('#c4').show();
     });
 });
